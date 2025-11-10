180f7091b1a03f04c46c50dbd782b55d8ff82bb1
platypus-perry03 <choe31904@gmail.com>
2025-11-10 19:32:22 +0900

    Updated Changes

---
+++ sunmoon-shuttle/bin/sunmoon_shuttle.dart
@@ -0,0 +1,5 @@
+import 'package:sunmoon_shuttle/sunmoon_shuttle.dart' as sunmoon_shuttle;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sunmoon_shuttle.calculate()}!');
+}
---
+++ sunmoon-shuttle/test/sunmoon_shuttle_test.dart
@@ -0,0 +1,8 @@
+import 'package:sunmoon_shuttle/sunmoon_shuttle.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
