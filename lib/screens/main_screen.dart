09cb37082f9c4e504ef32d1a8d0510d79885627d
platypus-perry03 <choe31904@gmail.com>
2025-11-26 15:24:59 +0900

    Updated Changes

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -209,6 +209,14 @@ class _MainScreenState extends State<MainScreen> {
                                     zoom: 15,
                                   ),
                                   mapType: NMapType.basic,
+                                  minZoom: 10.0,
+                                  maxZoom: 20.0,
+                                  scrollGesturesEnabled: true,
+                                  zoomGesturesEnabled: true,
+                                  tiltGesturesEnabled: true,
+                                  rotateGesturesEnabled: true,
+                                  locationButtonEnable: false,
+                                  consumeSymbolTapEvents: false,
                                 ),
                                 onMapReady: (controller) async {
                                   _mapController = controller;
